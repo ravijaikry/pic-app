e8781a8bfb8ef067ae2a3164341a934274945bc1
ravi <ravi@ravi-Inspiron-N4030.(none)>
2013-02-21 23:56:03 -0500

    sample app

---
+++ pic-app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PicApp::Application.config.secret_token = '713ff3cc94a8e4043c9c78f98569a40da00daf036ebe2bb83ad1f4fc0152ae72cc50d50886785fcc961941eba18f305ac22aabc156c5c7203fecfd34ec5563a3'
